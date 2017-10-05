03c7f8f0a4f671ff21472b69eaad62416634f5ca
Oleg Tsarkov <olegtsts@gmail.com>
2017-10-05 11:37:35 +0300

    Change binaries names in test

--- special/ww-string/compare-brute-against-generated.cpp
+++ special/ww-string/compare-brute-against-generated.cpp
@@ -5,15 +5,15 @@
 int main() {
     for (int i = 0; i < 100; ++i) {
         std::ofstream fh("input");
-        int k = rand() % 3 + 1;
+        int k = rand() % 5 + 1;
         int n = rand() % 7 + 1;
-        int a = rand() % 3 + 1;
+        int a = rand() % 5 + 1;
         fh << k << " " << n << " " << a << std::endl;
         fh.close();
-        if (system("./1.out < input > 1.output")) {
+        if (system("./brute.out < input > 1.output")) {
             throw std::logic_error("test programm failed");
         }
-        if (system("./2.out < input > 2.output 2>/dev/null")) {
+        if (system("./generated.out < input > 2.output 2>/dev/null")) {
             throw std::logic_error("tested programm failed");
         }
         if (system("diff 1.output 2.output")) {
