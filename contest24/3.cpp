3cb594192f1bfec287f3f68bd0d3a07665072751
Oleg Tsarkov <olegtsts@gmail.com>
2017-11-26 22:47:02 +0300

    Precalculating primes

--- contest24/3.cpp
+++ contest24/3.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
 long long int MODULO = 1000000007;
 
@@ -7,33 +8,38 @@ int main() {
     std::ios::sync_with_stdio(false);
     std::vector<int> primes{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67};
     std::vector<long long int> counts(524288, 0);
+    std::vector<long long int> new_counts(524288, 0);
     counts[0] = 1;
     int n;
     std::cin >> n;
-    for (int i = 0; i < n; ++i) {
-        int a = 0;
-        std::cin >> a;
-        int total_xor = 0;
+    std::vector<long long int> masks(71, 0);
+    for (size_t number = 1; number <= 70; ++number) {
+        size_t a = number;
         for (size_t prime_index = 0; prime_index < primes.size(); ++prime_index) {
             auto prime = primes[prime_index];
-            int count_of_prime = 0;
+            size_t count_of_prime = 0;
             while (a % prime == 0) {
                 a /= prime;
                 ++count_of_prime;
             }
             if (count_of_prime % 2 == 1) {
-                total_xor ^= (1 << prime_index);
+                masks[number] ^= (1 << prime_index);
             }
             if (a == 1) {
                 break;
             }
         }
+    }
+
+    for (int i = 0; i < n; ++i) {
+        int a = 0;
+        std::cin >> a;
+        long long int total_xor = masks[a];
         for (size_t i = 0; i < 524288; ++i) {
-            if (i > (i ^ total_xor)) {
-                counts[i] += counts[i ^ total_xor];
-                counts[i] %= MODULO;
-            }
+            new_counts[i] = counts[i] + counts[i ^ total_xor];
+            new_counts[i] %= MODULO;
         }
+        std::swap(counts, new_counts);
     }
     std::cout << (counts[0] + MODULO - 1) % MODULO << std::endl;
     return 0;
