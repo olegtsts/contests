8f02fd60a1b1143fc7137605b720902ddd1ed103
Oleg Tsarkov <olegtsts@gmail.com>
2020-04-25 12:40:20 +0000

    Adding 1

---
+++ contest61/1.cpp
@@ -0,0 +1,125 @@
+#include <bits/stdc++.h>
+#include <cstdlib>
+#include <string>
+#include <limits>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ostream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the removeDuplicates function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+SinglyLinkedListNode* removeDuplicates(SinglyLinkedListNode* head) {
+    SinglyLinkedListNode* old_head = head;
+    SinglyLinkedListNode* current_node = head;
+    while (current_node != nullptr) {
+        SinglyLinkedListNode* next_node = current_node->next;
+        while (next_node != nullptr && next_node->data == current_node->data) {
+            SinglyLinkedListNode* cur_next_node = next_node;
+            next_node = next_node->next;
+            free(cur_next_node);
+        }
+        current_node->next = next_node;
+        current_node = next_node;
+    }
+    return old_head;
+}
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        SinglyLinkedList* llist = new SinglyLinkedList();
+
+        int llist_count;
+        cin >> llist_count;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        for (int i = 0; i < llist_count; i++) {
+            int llist_item;
+            cin >> llist_item;
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+            llist->insert_node(llist_item);
+        }
+
+        SinglyLinkedListNode* llist1 = removeDuplicates(llist->head);
+
+        print_singly_linked_list(llist1, " ", std::cout);
+        std::cout << "\n";
+
+        free_singly_linked_list(llist1);
+    }
+
+
+    return 0;
+}
+
